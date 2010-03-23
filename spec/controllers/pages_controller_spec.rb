8fea03fdc92ad3e8a538465d4fb2f7a254ba619c
Brian Swedenberg <bjswede@email.unc.edu>
2010-03-23 15:51:14 -0400

    testing

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+	integrate_views
 
   describe "GET 'home'" do
     it "should be successful" do
+		get 'home'
+		response.should be_success
+	end
+	
+	it "should have the right title" do
       get 'home'
-      response.should be_success
+      response.should have_tag("title", 
+								"Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -14,6 +21,12 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+	
+	it "should have the right title" do
+      get 'contact'
+      response.should have_tag("title", 
+								"Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
   
   describe "GET 'about'" do
@@ -21,5 +34,11 @@ describe PagesController do
 		get 'about'
 		response.should be_success
 	end
+	
+	it "should have the right title" do
+      get 'about'
+      response.should have_tag("title", 
+								"Ruby on Rails Tutorial Sample App | About")
+    end
   end
 end
