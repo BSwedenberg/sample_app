36e6983c7a68f272886df70f6c52f326754e2fd0
Brian Swedenberg <bjswede@email.unc.edu>
2010-03-24 16:18:00 -0400

    rerouting

---
+++ spec/integration/layout_links_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Layout links" do
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should render_template('pages/home')
+  end
+
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+    response.should render_template('pages/contact')
+  end
+
+  it "should have an About page at '/about'" do
+    get '/about'
+    response.should render_template('pages/about')
+  end
+
+end
